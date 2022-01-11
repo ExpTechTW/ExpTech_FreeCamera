ad6591aaa87ce418be20b368cc4a82f75b6626cc
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-01-11 22:57:05 +0800

    Add files via upload

--- src/main/java/commands/commands.java
+++ src/main/java/commands/commands.java
@@ -19,17 +19,15 @@ public class commands implements CommandExecutor{
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(Objects.equals(label, "et")) {
-            if (Objects.equals(args[0], "freecamera")) {
-                if (Objects.equals(args[1], "reload")) {
+        if(Objects.equals(label, "fc")) {
+                if (Objects.equals(args[0], "reload")) {
                     plugin.getServer().getPluginManager().disablePlugin(plugin);
                     plugin.getServer().getPluginManager().enablePlugin(plugin);
-                } else if (Objects.equals(args[1], "disable")) {
+                } else if (Objects.equals(args[0], "disable")) {
                     plugin.getServer().getPluginManager().disablePlugin(plugin);
                 } else {
                     logger.log("WARN", "Core_onCommand", "Unknown Command");
                 }
-            }
         }
         if(Objects.equals(label, "cam")) {
             if(!sender.getName().equals("CONSOLE")) {
--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,7 +1,11 @@
 package main;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import commands.commands;
 import core.logger;
+import core.network;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,11 +25,16 @@ public class main extends JavaPlugin implements Listener {
     public void onEnable() {
         if(VersionCode>=Support) {
             folder = getDataFolder();
+            Objects.requireNonNull(getCommand("fc")).setExecutor(new commands(this));
             Objects.requireNonNull(getCommand("cam")).setExecutor(new commands(this));
             logger.log("INFO", "FreeCamera_onEnable", "Loading Success! - Designed by ExpTech.tw!");
         }else {
             logger.log("WARN","FreeCamera_onEnable","Please update your Core version");
             Bukkit.getPluginManager().disablePlugins();
         }
+    }
+    @Override
+    public void onDisable(){
+        logger.log("INFO","FreeCamera_onDisable","Closing! Version: "+getDescription().getVersion());
     }
 }
